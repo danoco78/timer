422935a59f1a5cbd822d987cbd94a73e280cba8c
danoco78 <danoco78@gmail.com>
2021-04-18 23:05:52 -0500

    interfaz grafica

---
+++ lib/widget/my_button.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  final Color color;
+  final String text;
+
+  final Function onPressed;
+
+  const MyButton(
+      {@required this.color, @required this.text, @required this.onPressed})
+      : super();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      onPressed: this.onPressed,
+      color: this.color,
+      textColor: Colors.white,
+      child: Text(this.text),
+    );
+  }
+}
